9c534ef5ba0716f4f141fc43b38b1b41067ad133
Ricardo Rubio <ricardo.rubio@alumnos.uneatlantico.es>
2025-06-24 19:51:08 +0200

    borrar el do while del main

--- entregas/rubioRicardo/src/Final.java
+++ entregas/rubioRicardo/src/Final.java
@@ -26,29 +26,6 @@ public class Final {
         problema3 = (sumaTotalCarnet / 6) + 1;
 
        ajustarProblemas(problema1, problema2, problema3);
-
-        do {
-            if (problema1 == problema2) {
-                problema1 = problema1 + 1;
-            }
-            if (problema1 == problema3) {
-                problema1 = problema1 + 1;
-            }
-            if (problema2 == problema3) {
-                problema3 = problema3 + 1;
-            }
-            if (problema1 == 7) {
-                problema1 = 1;
-            }
-            if (problema2 == 7) {
-                problema2 = 1;
-            }
-            if (problema3 == 7) {
-                problema3 = 1;
-            }
-        } while (problema1 == problema2 || problema1 == problema3 || problema2 == problema3 || problema1 > 6
-                || problema2 > 6 || problema3 > 6);
-
         System.out.println("Problema 1: " + problema1);
         System.out.println("Problema 2: " + problema2);
         System.out.println("Problema 3: " + problema3);
